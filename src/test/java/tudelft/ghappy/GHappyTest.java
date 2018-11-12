23fd41280a8c508532d505d386c357ac21b8062c
Alex <alexander-hentschel@gmx.net>
2018-11-12 13:58:20 +0100

    add tests for the GHappy-class
    resolve the bug in the GHappy-class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,37 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name="input={0}, expectedResult={1}")
+    @CsvSource({"g,false","gg,true"})
+    public void justGs(String g, boolean expectedResult){
+        boolean result = new GHappy().gHappy(g);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @ParameterizedTest(name="input={0}, expectedResult={1}")
+    @CsvSource({"xxgxx,false","xxggxx,true"})
+    public void xAndGInAString(String g, boolean expectedResult){
+        boolean result = new GHappy().gHappy(g);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @ParameterizedTest(name="input={0}, expectedResult={1}")
+    @CsvSource({"xxgxggx,false","xggxggx,true"})
+    public void gIsolated(String g, boolean expectedResult){
+        boolean result = new GHappy().gHappy(g);
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+
+    @ParameterizedTest(name="input={0}, expectedResult={1}")
+    @CsvSource({"gxx,false","ggxx,true"})
+    public void gAtFirstPosition(String g, boolean expectedResult){
+        boolean result = new GHappy().gHappy(g);
+        Assertions.assertEquals(expectedResult,result);
+    }
 }
