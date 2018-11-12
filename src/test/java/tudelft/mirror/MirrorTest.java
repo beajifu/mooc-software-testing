0c0b864ab0606444e8c0e9aa756520323bf2a6f6
Alex <alexander-hentschel@gmx.net>
2018-11-12 13:42:46 +0100

    add tests for the Mirror-class
    resolve the bug in the Mirror-class

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,41 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({"abXYba,ab", "aXYa,a"})
+    public void evenCharsAndNonOverlapping(String input, String expectedResult){
+        Mirror m = new Mirror();
+        String result = m.mirrorEnds(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({"abXYZba,ab", "aXYZa,a"})
+    public void oddCharsAndNonOverlapping(String input, String expectedResult){
+        Mirror m = new Mirror();
+        String result = m.mirrorEnds(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({"abba,abba", "aa,aa"})
+    public void evenCharsAndOverlapping(String input, String expectedResult){
+        Mirror m = new Mirror();
+        String result = m.mirrorEnds(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({"aba,aba", "a,a"})
+    public void oddCharsAndOverlapping(String input, String expectedResult){
+        Mirror m = new Mirror();
+        String result = m.mirrorEnds(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
